05b2fe131a8a9a7ba3c0bf4b06cdb754d35e909f
AndyChoi4495 <yun4495@gmail.com>
2021-10-27 22:57:10 +0900

    design ing~

---
+++ routes/admin/order-router.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const { error } = require('../../modules/util');
+
+router.get('/', (req, res, next) => {
+  res.render('admin/order/order-list', { css: 'admin-order' });
+});
+
+router.post('/', (req, res, next) => {
+  res.send('/admin/order:POST');
+});
+
+router.put('/', (req, res, next) => {
+  res.send('/admin/order:PUT');
+});
+
+router.delete('/', (req, res, next) => {
+  res.send('/admin/order:DELETE');
+});
+
+module.exports = { name: '/order', router };
---
+++ routes/admin/prd-router.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const { error } = require('../../modules/util');
+
+router.get('/', (req, res, next) => {
+  res.render('admin/prd/prd-list', { css: 'admin-prd' });
+});
+
+router.post('/', (req, res, next) => {
+  res.send('/admin/prd:POST');
+});
+
+router.put('/', (req, res, next) => {
+  res.send('/admin/prd:PUT');
+});
+
+router.delete('/', (req, res, next) => {
+  res.send('/admin/prd:DELETE');
+});
+
+router.get('/cate', (req, res, next) => {
+  res.render('admin/prd/cate-list', { css: 'admin-cate' });
+});
+
+module.exports = { name: '/prd', router };
